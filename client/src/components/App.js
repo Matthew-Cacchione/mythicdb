2f0160d7bb05ba2504ed94d1c0f9fa3c7c8be335
Matthew-Cacchione <matthew.cacchione@gmail.com>
2022-07-27 08:50:21 -0400

    add basic routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import GlobalStyles from "./GlobalStyles";
+
 const App = () => {
-  return <>App</>;
+  return (
+    <Router>
+      <GlobalStyles />
+      <Switch>
+        <Route path="/">Home</Route>
+        <Route path="">No path found.</Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;

db3827e58752a8ebde1f23f950fb5c2879b27b4e
Matthew-Cacchione <matthew.cacchione@gmail.com>
2022-08-05 13:35:20 -0400

    create sign in endpoint

--- server/handlers/authentication.js
+++ server/handlers/authentication.js
@@ -30,7 +30,11 @@ const getUser = async (req, res) => {
 
     // Verify if the user was found and respond appropriately.
     if (user) {
-      return res.status(200).json({ status: 200, data: { ...user } });
+      // Remove the user's password from the response.
+      const clone = { ...user };
+      delete clone.password;
+
+      return res.status(200).json({ status: 200, data: { user: clone } });
     } else {
       return res.status(404).json({
         status: 404,
@@ -39,7 +43,7 @@ const getUser = async (req, res) => {
       });
     }
   } catch (e) {
-    console.error("Error occurred fetching user:", e);
+    console.error("Error fetching user:", e);
 
     // Respond based on the error thrown.
     switch (e.name) {
@@ -61,4 +65,58 @@ const getUser = async (req, res) => {
   }
 };
 
-module.exports = { getUser };
+const signIn = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+
+  // Extract the sign in details from the request.
+  const { email, password } = req.body;
+
+  // If either value is missing, respond with a bad request.
+  if (!email || !password) {
+    return res.status(400).json({
+      status: 400,
+      message: "Request is missing data.",
+      data: req.body,
+    });
+  }
+
+  try {
+    await client.connect();
+    const users = client.db("master").collection("users");
+
+    const user = await users.findOne({ email });
+
+    // Verify that the user attempting to sign in exists.
+    if (!user) {
+      return res.status(404).json({
+        status: 404,
+        message: "No user found with that email.",
+        data: { email },
+      });
+    }
+
+    // Verify that the password entered is correct.
+    const verify = await bcrypt.compare(password, user.password);
+
+    if (!verify) {
+      return res.status(401).json({
+        status: 401,
+        message: "Incorrect password provided.",
+        data: { email },
+      });
+    } else {
+      // Remove the user's password from the response.
+      const clone = { ...user };
+      delete clone.password;
+
+      return res.status(200).json({ status: 200, data: { user: clone } });
+    }
+  } catch (e) {
+    console.error("Error signing in:", e);
+    return res.status(500).json({ status: 500, message: e.name });
+  } finally {
+    client.close();
+  }
+};
+
+module.exports = { getUser, signIn };
--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -2,9 +2,14 @@
 const router = require("express").Router();
 
 // Require authentication handlers.
-const { getUser } = require("../handlers/authentication");
+const { getUser, signIn } = require("../handlers/authentication");
 
 // #Endpoints.
+
+// Get the user data given an id.
 router.get("/api/users/:_id", getUser);
+
+// Log the user in.
+router.post("/api/users/signIn", signIn);
 
 module.exports = router;

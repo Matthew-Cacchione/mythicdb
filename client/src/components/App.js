d31c35c26c4c29fb8a57339cf81074ef8bf22d72
Matthew-Cacchione <matthew.cacchione@gmail.com>
2022-08-05 09:15:04 -0400

    create auth0 provider

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Auth0Provider from "../authentication/auth0-navigate-provider";
 import GlobalStyles from "./GlobalStyles";
 import Header from "./Header";
 import Home from "./Home";
@@ -9,23 +10,25 @@ import { STRINGS } from "../constants";
 const App = () => {
   return (
     <Router>
-      <GlobalStyles />
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route
-          exact
-          path={STRINGS.paths.newPlayer}
-          element={<>Introduction</>}
-        />
-        <Route exact path={STRINGS.paths.affixes} element={<>Affixes</>} />
-        <Route
-          exact
-          path={STRINGS.paths.leaderboard}
-          element={<>Leaderboard</>}
-        />
-        <Route path="" element={<>No path found.</>} />
-      </Routes>
+      <Auth0Provider>
+        <GlobalStyles />
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route
+            exact
+            path={STRINGS.paths.newPlayer}
+            element={<>Introduction</>}
+          />
+          <Route exact path={STRINGS.paths.affixes} element={<>Affixes</>} />
+          <Route
+            exact
+            path={STRINGS.paths.leaderboard}
+            element={<>Leaderboard</>}
+          />
+          <Route path="" element={<>No path found.</>} />
+        </Routes>
+      </Auth0Provider>
     </Router>
   );
 };

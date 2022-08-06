571438acea02d6a92ec4829f85c4ea9bc13fbc40
Matthew-Cacchione <matthew.cacchione@gmail.com>
2022-08-06 09:19:55 -0400

    remove auth0 dependency

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Auth0Provider from "../authentication/auth0-navigate-provider";
 import GlobalStyles from "./GlobalStyles";
 import Header from "./Header";
 import Home from "./Home";
@@ -10,25 +9,23 @@ import { STRINGS } from "../constants";
 const App = () => {
   return (
     <Router>
-      <Auth0Provider>
-        <GlobalStyles />
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route
-            exact
-            path={STRINGS.paths.newPlayer}
-            element={<>Introduction</>}
-          />
-          <Route exact path={STRINGS.paths.affixes} element={<>Affixes</>} />
-          <Route
-            exact
-            path={STRINGS.paths.leaderboard}
-            element={<>Leaderboard</>}
-          />
-          <Route path="" element={<>No path found.</>} />
-        </Routes>
-      </Auth0Provider>
+      <GlobalStyles />
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route
+          exact
+          path={STRINGS.paths.newPlayer}
+          element={<>Introduction</>}
+        />
+        <Route exact path={STRINGS.paths.affixes} element={<>Affixes</>} />
+        <Route
+          exact
+          path={STRINGS.paths.leaderboard}
+          element={<>Leaderboard</>}
+        />
+        <Route path="" element={<>No path found.</>} />
+      </Routes>
     </Router>
   );
 };

452cce7ea55ca74ee92c19c861ef31e032be204a
Matthew-Cacchione <matthew.cacchione@gmail.com>
2022-08-10 09:36:50 -0400

    remove unused endpoints

--- server/routes/characters.js
+++ server/routes/characters.js
@@ -2,14 +2,11 @@
 const router = require("express").Router();
 
 // Require character handlers.
-const { getCharacter, getSuggestions } = require("../handlers/characters");
+const { getCharacter } = require("../handlers/characters");
 
 // #Endpoints.
 
 // Get the character data.
 router.get("/api/characters", getCharacter);
-
-// Get an array of character suggestions.
-router.get("/api/characters/suggestions", getSuggestions);
 
 module.exports = router;

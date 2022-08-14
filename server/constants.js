40a584cfb9f941f7082f62e1e0f071a58e550eca
Matthew-Cacchione <matthew.cacchione@gmail.com>
2022-08-14 17:09:55 -0400

    use higher quality icon assets

--- server/constants.js
+++ server/constants.js
@@ -1,39 +1,35 @@
 // Track the current affix rotation.
 const AFFIX_ROTATION = [10, 8, 12, 131];
 
-// Affix data.
+// Affix data, icons are provided courtesy of https://github.com/AcidWeb.
 const AFFIXES = [
   {
     id: 8,
     name: "Sanguine",
     description:
       "When slain, non-boss enemies leave behind a lingering pool of ichor that heals their allies and damages players.",
-    imgSrc:
-      "https://render.worldofwarcraft.com/us/icons/56/spell_shadow_bloodboil.jpg",
+    imgSrc: "/assets/icon-sanguine.png",
   },
   {
     id: 10,
     name: "Fortified",
     description:
       "Non-boss enemies have 20% more health and inflict up to 30% increased damage.",
-    imgSrc:
-      "https://render.worldofwarcraft.com/us/icons/56/ability_toughness.jpg",
+    imgSrc: "/assets/icon-fortified.png",
   },
   {
     id: 12,
     name: "Grievous",
     description:
       "Injured players suffer increasing damage over time until healed.",
-    imgSrc:
-      "https://render.worldofwarcraft.com/us/icons/56/ability_backstab.jpg",
+    imgSrc: "/assets/icon-grievous.png",
   },
   {
     id: 131,
     name: "Shrouded",
     description:
       "Nathrezim infiltrators have disguised themselves among enemies throughout the dungeon. Cartel Ta will reward you handsomely for assisting in their capture.",
-    imgSrc:
-      "https://render.worldofwarcraft.com/us/icons/56/spell_shadow_nethercloak.jpg",
+    imgSrc: "/assets/icon-shrouded.png",
   },
 ];
 

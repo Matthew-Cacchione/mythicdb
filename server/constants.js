4b30e4364227a87db3a6dedfd5e64b4d66830ba0
Matthew-Cacchione <matthew.cacchione@gmail.com>
2022-08-16 07:05:28 -0400

    update weekly affixes

--- server/constants.js
+++ server/constants.js
@@ -1,14 +1,28 @@
 // Track the current affix rotation.
-const AFFIX_ROTATION = [10, 8, 12, 131];
+const AFFIX_ROTATION = [9, 7, 13, 131];
 
 // Affix data, icons are provided courtesy of https://github.com/AcidWeb.
 const AFFIXES = [
+  {
+    id: 7,
+    name: "Bolstering",
+    description:
+      "When any non-boss enemy dies, its death cry empowers nearby allies, temporarily increasing their maximum health by 15% and damage by 20%.",
+    imgSrc: "/assets/icon-bolstering.png",
+  },
   {
     id: 8,
     name: "Sanguine",
     description:
       "When slain, non-boss enemies leave behind a lingering pool of ichor that heals their allies and damages players.",
     imgSrc: "/assets/icon-sanguine.png",
+  },
+  {
+    id: 9,
+    name: "Tyrannical",
+    description:
+      "Bosses have 30% more health. Bosses and their minions inflict up to 15% increased damage.",
+    imgSrc: "/assets/icon-tyrannical.png",
   },
   {
     id: 10,
@@ -23,6 +37,13 @@ const AFFIXES = [
     description:
       "Injured players suffer increasing damage over time until healed.",
     imgSrc: "/assets/icon-grievous.png",
+  },
+  {
+    id: 13,
+    name: "Explosive",
+    description:
+      "While in combat, enemies periodically summon Explosive Orbs that will detonate if not destroyed.",
+    imgSrc: "/assets/icon-explosive.png",
   },
   {
     id: 131,

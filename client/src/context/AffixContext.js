60f175a6eff1535a89af60016b3d58f4eafb5a5f
Matthew-Cacchione <matthew.cacchione@gmail.com>
2022-08-26 10:46:12 -0400

    create affix context

---
+++ client/src/context/AffixContext.js
@@ -0,0 +1,40 @@
+import { createContext, useReducer } from "react";
+
+const initialState = {
+  data: null,
+  hasLoaded: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "affixes-fetched":
+      return {
+        ...state,
+        data: action.data,
+        hasLoaded: true,
+      };
+
+    default:
+      throw new Error(`Unrecognized action: ${action.type}`);
+  }
+};
+
+export const AffixContext = createContext(null);
+
+export const AffixProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Indicate that affix data has been fetched from the API.
+  const affixesFetched = (data) => {
+    dispatch({
+      type: "affixes-fetched",
+      data,
+    });
+  };
+
+  return (
+    <AffixContext.Provider value={{ state, actions: { affixesFetched } }}>
+      {children}
+    </AffixContext.Provider>
+  );
+};

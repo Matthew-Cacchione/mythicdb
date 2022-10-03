53f94bed5bb97740f4965ed68f72cd4c2723cfe0
Matthew-Cacchione <matthew.cacchione@gmail.com>
2022-10-03 08:10:26 -0400

    update loading indicator

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -3,28 +3,56 @@ import styled, { keyframes } from "styled-components";
 const Loader = () => {
   return (
     <Wrapper>
-      <Circle />
+      <Spinner>
+        <div />
+        <div />
+        <div />
+        <div />
+      </Spinner>
     </Wrapper>
   );
 };
 
 const spin = keyframes`
-from {
-    transform: rotate(0);
+0% {
+  transform: rotate(0deg);
 }
 
-to {
-    transform: rotate(360deg);
+100% {
+  transform: rotate(360deg);
 }
 `;
 
-const Circle = styled.div`
-  animation: ${spin} 2s linear;
-  border: 8px solid var(--color-background);
-  border-radius: 50%;
-  border-top: 8px solid var(--color-primary);
-  height: 60px;
-  width: 60px;
+const Spinner = styled.div`
+  display: inline-block;
+  height: 80px;
+  position: relative;
+  width: 80px;
+
+  & div {
+    animation: ${spin} 2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border: 6px solid var(--color-primary);
+    border-color: var(--color-primary) transparent transparent transparent;
+    border-radius: 50%;
+    box-sizing: border-box;
+    display: block;
+    height: 64px;
+    margin: 8px;
+    position: absolute;
+    width: 64px;
+  }
+
+  & div:nth-child(1) {
+    animation-delay: -550ms;
+  }
+
+  & div:nth-child(2) {
+    animation-delay: -400ms;
+  }
+
+  & div:nth-child(3) {
+    animation-delay: -250ms;
+  }
 `;
 
 const Wrapper = styled.div`

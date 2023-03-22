554d7e6395c703960abd8c851f400036de502093
matthew-cacchione <matthew.cacchione@gmail.com>
2023-03-22 08:48:14 -0400

    clean up helpers

--- server/src/helpers/strings.ts
+++ server/src/helpers/strings.ts
@@ -1,7 +1,7 @@
 // Capitalize the string, e.g. heLlO becomes Hello.
-const capitalize = (str: String) => {
-  const capital = str.charAt(0).toUpperCase();
-  const lower = str.slice(1).toLowerCase();
+const capitalize = (string: String) => {
+  const capital = string.charAt(0).toUpperCase();
+  const lower = string.slice(1).toLowerCase();
 
   return capital + lower;
 };

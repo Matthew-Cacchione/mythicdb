ad397b9b02ec141693b1ec88c620c0bb1f953d2c
matthew-cacchione <matthew.cacchione@gmail.com>
2023-06-30 09:38:18 -0400

    prepare client for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "./build",
+  },
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-      "/api": "http://127.0.0.1:8000",
+      "/api": "https://mythicdb.onrender.com",
     },
   },
 });
